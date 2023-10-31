f2404ce7301c802b6bc38f31b93873e60adc3be9
Paul mazeau <mazeau.paul@gmail.com>
2023-10-31 17:40:35 +0100

    Comparaison json ImmoJeune

--- Javascript/Scrapping_Scripts/Recherche/ImmoJeune_Recherche.js
+++ Javascript/Scrapping_Scripts/Recherche/ImmoJeune_Recherche.js
@@ -1,9 +1,18 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
 function getCurrentDateString() {
     const date = new Date();
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
+
+function getOldData(filename) {
+    try {
+        return JSON.parse(fs.readFileSync(filename, 'utf-8'));
+    } catch (e) {
+        return [];
+    }
 }
 
 (async () => {
@@ -27,8 +36,6 @@ function getCurrentDateString() {
             break;
         }
     }
-
-    
 
     const allData = await page.evaluate(() => {
         const cards = [...document.querySelectorAll('.card.col')];
@@ -58,12 +65,30 @@ function getCurrentDateString() {
 
     console.log(`Scrapped ${allData.length} ads from ${url}`);
 
-    // Choisir le nom et l'emplacement de la sortie du fichier json
-    const fileName = `../../Resultat_Recherche/ImmoJeune_Recherche/Data_ImmoJeune_Recherche_${getCurrentDateString()}.json`;
+    const currentDate = getCurrentDateString();
+    const previousDate = new Date();
+    previousDate.setDate(previousDate.getDate() - 1);
+    const previousDateString = `${previousDate.getFullYear()}-${String(previousDate.getMonth() + 1).padStart(2, '0')}-${String(previousDate.getDate()).padStart(2, '0')}`;
 
-    // Créer le fichier json
-    fs.writeFileSync(fileName, JSON.stringify(allData, null, 2), 'utf-8');
-    console.log(`Data saved to ${fileName}!`);
+    const oldFileName = path.join(__dirname, `../../Resultat_Recherche/ImmoJeune_Recherche/Data_ImmoJeune_Recherche_${previousDateString}.json`);
+    const oldData = getOldData(oldFileName);
+
+    const newAnnouncements = allData.filter(item => !oldData.some(oldItem => oldItem.link === item.link));
+    const removedAnnouncements = oldData.filter(item => !allData.some(newItem => newItem.link === item.link));
+    const updatedData = allData.filter(item => !removedAnnouncements.some(removedItem => removedItem.link === item.link));
+
+    const outputFileName = path.join(__dirname, `../../Resultat_Recherche/ImmoJeune_Recherche/Data_ImmoJeune_Recherche_${currentDate}.json`);
+    const updatedFileName = path.join(__dirname, `../../Resultat_Recherche/Up_To_Date_Recherche/Updated_Data_ImmoJeune_Recherche_${currentDate}.json`);
+
+    fs.writeFileSync(outputFileName, JSON.stringify(allData, null, 2), 'utf-8');
+    fs.writeFileSync(updatedFileName, JSON.stringify(updatedData, null, 2), 'utf-8');
+
+    console.log(`Total scraped ads: ${allData.length}`);
+    console.log(`Today's data saved to ${outputFileName}`);
+    console.log(`${newAnnouncements.length} new ad(s).`);
+    console.log(`${removedAnnouncements.length} removed ad(s).`);
+    console.log(`${updatedData.length - newAnnouncements.length} retained ad(s).`);
+    console.log(`Updated data saved to ${updatedFileName}`);
 
     await browser.close();
 })();
