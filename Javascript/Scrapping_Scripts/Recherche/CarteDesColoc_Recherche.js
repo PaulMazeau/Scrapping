6f8ab6207d6bde5bb8b6ce8c5d3db6efe5eed8d7
Paul MAZEAU <mazeau.paul@gmail.com>
2023-11-20 10:37:45 +0100

    Carte des colocs

--- Javascript/Scrapping_Scripts/Recherche/CarteDesColoc_Recherche.js
+++ Javascript/Scrapping_Scripts/Recherche/CarteDesColoc_Recherche.js
@@ -1,62 +1,20 @@
 const axios = require('axios');
 const fs = require('fs');
-const path = require('path');
-const moment = require('moment');
 
-const getOldData = filename => {
-    try {
-        return JSON.parse(fs.readFileSync(filename, 'utf-8'));
-    } catch (e) {
-        return [];
-    }
-};
-
-const baseUrl = 'https://www.lacartedescolocs.fr/listing_search/list_results';
-const params = {
-    viewport: JSON.stringify({
-        canonical_path: "/fr/ile-de-france/paris",
-        city: "Paris",
-        county: "Paris",
-        administrative: "Île-de-France",
-        country_code: "fr",
-        sw_lat: 48.815575,
-        sw_lon: 2.224122,
-        ne_lat: 48.902156,
-        ne_lon: 2.469703
-    }),
-    filters: JSON.stringify({
-        offset: 0,
-        sortBy: "published_at DESC",
-        currency: "EUR"
-    })
-};
-
-const url = `${baseUrl}?${new URLSearchParams(params).toString()}`;
+// URL de la requête
+const url = 'https://www.lacartedescolocs.fr/listing_search/list_results?viewport=%7B%22canonical_path%22%3A%22%2Ffr%2File-de-france%2Fparis%22%2C%22city%22%3A%22Paris%22%2C%22county%22%3A%22Paris%22%2C%22administrative%22%3A%22%C3%8Ele-de-France%22%2C%22country_code%22%3A%22fr%22%2C%22sw_lat%22%3A48.815575%2C%22sw_lon%22%3A2.224122%2C%22ne_lat%22%3A48.902156%2C%22ne_lon%22%3A2.469703%7D&filters=%7B%22offset%22%3A0%2C%22sortBy%22%3A%22published_at%20DESC%22%2C%22currency%22%3A%22EUR%22%7D';
 
 axios.get(url)
-    .then(async (response) => {
-        const currentDateString = moment().format('YYYY-MM-DD'); // Utilisez moment pour obtenir la date au format désiré
-        const resultsToday = response.data.data.res.results || [];
-
-        const previousDayString = moment().subtract(1, 'days').format('YYYY-MM-DD'); // Obtenez la date du jour précédent avec moment
-
-        const oldFileName = path.join(__dirname, '../../Resultat_Recherche/CarteDesColoc_Recherche', `Data_CarteDesColoc_Recherche_${previousDayString}.json`);        
-        const oldData = await getOldData(oldFileName);
-
-        const newAnnouncements = resultsToday.filter(item => !oldData.some(oldItem => oldItem._id === item._id));
-        const removedAnnouncements = oldData.filter(item => !resultsToday.some(newItem => newItem._id === item._id));
-        const updatedData = resultsToday.filter(item => !removedAnnouncements.some(removedItem => removedItem._id === item._id));
-
-        const outputFileName = path.join(__dirname, '../../Resultat_Recherche/CarteDesColoc_Recherche', `Data_CarteDesColoc_Recherche_${currentDateString}.json`);
-        const updatedFileName = path.join(__dirname, '../../Resultat_Recherche/Up_To_Date_Recherche/CarteDesColoc_Recherche_Up_To_Date', `Updated_Data_CarteDesColoc_Recherche_${currentDateString}.json`);
-
-        await fs.writeFile(outputFileName, JSON.stringify({ data: { res: { results: resultsToday } } }, null, 4), 'utf-8');
-        await fs.writeFile(updatedFileName, JSON.stringify({ data: { res: { results: updatedData } } }, null, 4), 'utf-8');
-
-        console.log(`Total d'annonces scrapées : ${resultsToday.length}`);
-        // console.log(`Données du jour sauvegardées dans ${outputFileName}!`);
-        // console.log(`TOTAL_NOUVELLES_ANNONCES:${newAnnouncements.length} nouvelles annonces sur Appartager.`);
-        // console.log(`${removedAnnouncements.length} annonce(s) supprimée(s).`);
-        // console.log(`${updatedData.length - newAnnouncements.length} annonce(s) conservée(s).`);
-        // console.log(`Données mises à jour sauvegardées dans ${updatedFileName}!`);
-    })
+  .then(response => {
+    // Stocker la réponse dans un fichier JSON
+    fs.writeFile('response.json', JSON.stringify(response.data, null, 2), 'utf8', (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      console.log('Les données ont été enregistrées dans response.json');
+    });
+  })
+  .catch(error => {
+    console.error('Erreur lors de la requête :', error);
+  });
