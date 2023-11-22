588bc06cc97ae2e65554c3df26b734bd3ad9aab6
Paul MAZEAU <mazeau.paul@gmail.com>
2023-11-22 11:47:40 +0100

    Normalisation Appartager

--- Javascript/Scrapping_Scripts/Annonce/Normalisation/Normalisation_Appartager_Annonce.js
+++ Javascript/Scrapping_Scripts/Annonce/Normalisation/Normalisation_Appartager_Annonce.js
@@ -5,41 +5,35 @@ function getCurrentDateString() {
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
 }
 
-// Spécifiez le chemin d'accès au fichier contenant votre JSON brut
 const rawDataPath = `../../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${getCurrentDateString()}.json`;
-
-// Lecture du fichier JSON brut
 let rawData = JSON.parse(fs.readFileSync(rawDataPath, 'utf8'));
 
 function normalizeData(data) {
+    const images = data.images ? data.images.map(img => img) : [];
+    const amenities = [];
+    const meuble = [];
+    const rooms = [];
+
     return {
-        title: data.title,
+        title: data.heading || '',
         location: {
-            address: data.address,
-            city: data.propertyDetails && data.propertyDetails["Ville :"] ? data.propertyDetails["Ville :"].replace(':', '').trim() : '',
-            postalCode: '', // Ajoutez le code postal si disponible
+            address: data.address || '',
+            city: data.address.split(' ').slice(-1)[0] || '', // Simplification, peut être améliorée
         },
-        images: data.images, // Si vous souhaitez conserver le tableau d'images tel quel
+        images: images,
         price: {
             rent: data.price.replace('€', '').trim(),
-            rentWithoutCharge: '', // Ajoutez si disponible
-            pricem2: data.propertyDetails && data.propertyDetails["Prix au m² :"] ? data.propertyDetails["Prix au m² :"].replace('€', '').trim() : '',
-            charge: '', // Ajoutez si disponible
-            deposit: data.propertyDetails && data.propertyDetails["Dépôt de garantie :"] ? data.propertyDetails["Dépôt de garantie :"].replace('€', '').trim() : '',
         },
-        furnished: data.propertyDetails && data.propertyDetails["Meublé :"] ? data.propertyDetails["Meublé :"].replace(':', '').trim() : 'Non',
-        type: data.propertyDetails && data.propertyDetails["Type de propriété:"] ? data.propertyDetails["Type de propriété:"].replace(':', '').trim() : '',
-        bedrooms: data.propertyDetails && data.propertyDetails["Pièces :"] ? data.propertyDetails["Pièces :"].replace(':', '').trim() : '',
-        bathrooms: data.propertyDetails && data.propertyDetails["Salles de bain :"] ? data.propertyDetails["Salles de bain :"].replace(':', '').trim() : '',
-        size: data.propertyDetails && data.propertyDetails["Surface :"] ? data.propertyDetails["Surface :"].replace('m2', '').trim() : '',
+        furnished: data.features.includes('Meublé') ? 'Oui' : 'Non',
+        type: data.features.includes('Appartement') ? 'Appartement' : '',
         description: data.description,
-        verified: data.verified === 'Oui',
+        amenities: amenities,
+        meuble: meuble,
+        rooms: rooms,
     };
 }
 
-// Normalisation de chaque annonce
 let normalizedDataArray = rawData.map(annonce => normalizeData(annonce));
 
-// Écriture des données normalisées dans un fichier (facultatif)
 const normalizedDataPath = `../../../Resultat_Annonce/Normalisation/Normalized_Data_Appartager_Annonces_${getCurrentDateString()}.json`;
 fs.writeFileSync(normalizedDataPath, JSON.stringify(normalizedDataArray, null, 2), 'utf8');
