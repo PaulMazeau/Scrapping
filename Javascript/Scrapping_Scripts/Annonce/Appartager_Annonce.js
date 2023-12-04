78a146b2a5fd6899dd007c4fcff623f00f2c3cd4
Paul mazeau <mazeau.paul@gmail.com>
2023-12-04 16:11:25 +0100

    Normalisation et comparaison Appartager Annnonce

--- Javascript/Scrapping_Scripts/Annonce/Appartager_Annonce.js
+++ Javascript/Scrapping_Scripts/Annonce/Appartager_Annonce.js
@@ -1,8 +1,15 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+const path = require('path');
 
 function getCurrentDateString() {
     const date = new Date();
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
+
+function getPreviousDateString() {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
 }
 
@@ -44,27 +51,48 @@ async function scrapePage(browser, url) {
 }
 
 (async () => {
-    const annoncesPath = `../../Resultat_Recherche/Up_To_Date_Recherche/Appartager_Recherche_Up_To_Date/Updated_Data_Appartager_Recherche_${getCurrentDateString()}.json`;
-    const annonces = JSON.parse(fs.readFileSync(annoncesPath, 'utf-8'));
-    const allData = []; // Initialiser le tableau pour stocker les données de toutes les annonces
+    const currentDate = getCurrentDateString();
+    const previousDate = getPreviousDateString();
 
-    const browser = await puppeteer.launch(); // Ouvrir un seul navigateur
+    const previousDataPath = path.join(__dirname, `../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${previousDate}.json`);
+    let previousData;
+    try {
+        previousData = JSON.parse(fs.readFileSync(previousDataPath, 'utf8'));
+    } catch (error) {
+        previousData = []; // Si le fichier du jour précédent n'existe pas
+    }
+
+    const annoncesPath = path.join(__dirname, `../../Resultat_Recherche/Up_To_Date_Recherche/Appartager_Recherche_Up_To_Date/Updated_Data_Appartager_Recherche_${currentDate}.json`);
+    const annonces = JSON.parse(fs.readFileSync(annoncesPath, 'utf-8'));
+    const allData = [];
+
+    const browser = await puppeteer.launch();
 
     for (let annonce of annonces) {
         try {
             const data = await scrapePage(browser, annonce.link);
-            allData.push(data); // Ajouter les données de chaque annonce au tableau
-            console.log('Annonce faite')
-            console.log(allData.length)
+            allData.push(data);
+            console.log('Annonce faite');
+            console.log(allData.length);
         } catch (error) {
             console.error(`Failed to scrape the page at ${annonce.link} due to: ${error}`);
         }
     }
 
-    await browser.close(); // Fermez le navigateur une fois toutes les pages visitées
+    await browser.close();
 
-    // Créer un nom de fichier basé sur la date actuelle
-    const fileName = `../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${getCurrentDateString()}.json`;
-    fs.writeFileSync(fileName, JSON.stringify(allData, null, 2), 'utf-8'); // Écrire toutes les données dans un seul fichier
+    const newAnnouncements = allData.filter(item => !previousData.some(oldItem => oldItem.link === item.link));
+    const removedAnnouncements = previousData.filter(item => !allData.some(newItem => newItem.link === item.link));
+    const upToDateAnnouncements = allData.filter(item => !newAnnouncements.includes(item));
+
+    const fileName = path.join(__dirname, `../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${currentDate}.json`);
+    const upToDateDataPath = path.join(__dirname, `../../Resultat_Annonce/Up_To_Date_Annonce/Appartager_Annonce_Up_To_Date/Updated_Data_Appartager_Annonces_${currentDate}.json`);
+
+    fs.writeFileSync(fileName, JSON.stringify(allData, null, 2), 'utf-8');
+    fs.writeFileSync(upToDateDataPath, JSON.stringify(upToDateAnnouncements, null, 2), 'utf-8');
+
     console.log(`All data saved to ${fileName}!`);
+    console.log(`TOTAL_NOUVELLES_ANNONCES:${newAnnouncements.length} nouvelles annonces sur Appartager.`);
+    console.log(`${removedAnnouncements.length} annonce(s) supprimée(s).`);
+    console.log(`${upToDateAnnouncements.length} annonce(s) à jour.`);
 })();
--- Javascript/Scrapping_Scripts/Annonce/Normalisation/Normalisation_Appartager_Annonce.js
+++ Javascript/Scrapping_Scripts/Annonce/Normalisation/Normalisation_Appartager_Annonce.js
@@ -1,12 +1,30 @@
 const fs = require('fs');
+const path = require('path');
 
 function getCurrentDateString() {
     const date = new Date();
     return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
 }
 
-const rawDataPath = `../../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${getCurrentDateString()}.json`;
+function getPreviousDateString() {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    return `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, '0')}-${String(date.getDate()).padStart(2, '0')}`;
+}
+
+const currentDate = getCurrentDateString();
+const previousDate = getPreviousDateString();
+
+const rawDataPath = path.join(__dirname, `../../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${currentDate}.json`);
 let rawData = JSON.parse(fs.readFileSync(rawDataPath, 'utf8'));
+
+const previousDataPath = path.join(__dirname, `../../../Resultat_Annonce/Appartager_Annonce/Data_Appartager_Annonces_${previousDate}.json`);
+let previousData;
+try {
+    previousData = JSON.parse(fs.readFileSync(previousDataPath, 'utf8'));
+} catch (error) {
+    previousData = [];
+}
 
 function normalizeData(data) {
     const images = data.images ? data.images.map(img => img) : [];
@@ -18,7 +36,7 @@ function normalizeData(data) {
         title: data.heading || '',
         location: {
             address: data.address || '',
-            city: data.address.split(' ').slice(-1)[0] || '', // Simplification, peut être améliorée
+            city: data.address.split(' ').slice(-1)[0] || '', 
         },
         images: images,
         price: {
@@ -35,5 +53,17 @@ function normalizeData(data) {
 
 let normalizedDataArray = rawData.map(annonce => normalizeData(annonce));
 
-const normalizedDataPath = `../../../Resultat_Annonce/Normalisation/Normalized_Data_Appartager/Normalized_Data_Appartager_Annonces_${getCurrentDateString()}.json`;
+const newAnnouncements = normalizedDataArray.filter(item => !previousData.some(oldItem => oldItem.link === item.link));
+const removedAnnouncements = previousData.filter(item => !normalizedDataArray.some(newItem => newItem.link === item.link));
+const upToDateAnnouncements = normalizedDataArray.filter(item => !newAnnouncements.includes(item));
+
+const normalizedDataPath = path.join(__dirname, `../../../Resultat_Annonce/Normalisation/Normalized_Data_Appartager/Normalized_Data_Appartager_Annonces_${currentDate}.json`);
+const upToDateDataPath = path.join(__dirname, `../../../Resultat_Annonce/Normalisation/Up_To_Date_Normalized/Appartager_Normalisation_Up_To_Date/Updated_Data_Appartager_Annonces_${currentDate}.json`);
+
 fs.writeFileSync(normalizedDataPath, JSON.stringify(normalizedDataArray, null, 2), 'utf8');
+fs.writeFileSync(upToDateDataPath, JSON.stringify(upToDateAnnouncements, null, 2), 'utf8');
+
+console.log(`Il y a ${normalizedDataArray.length} annonces sur Appartager.`);
+console.log(`TOTAL_NOUVELLES_ANNONCES:${newAnnouncements.length} nouvelles annonces sur Appartager.`);
+console.log(`${removedAnnouncements.length} annonce(s) supprimée(s).`);
+console.log(`${upToDateAnnouncements.length} annonce(s) à jour.`);
